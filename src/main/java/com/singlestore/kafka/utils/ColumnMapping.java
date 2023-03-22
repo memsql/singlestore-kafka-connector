b8566a145291ed8300f3efe45d7bfdc1f92c87dd
Adalbert Makarovych <amakarovych-ua@memsql.com>
2023-03-22 12:54:13 +0200

    check point

---
+++ src/main/java/com/singlestore/kafka/utils/ColumnMapping.java
@@ -0,0 +1,19 @@
+package com.singlestore.kafka.utils;
+
+public class ColumnMapping {
+  String columnName;
+  String fieldPath;
+
+  public ColumnMapping(String columnName, String fieldPath) {
+    this.columnName = columnName;
+    this.fieldPath = fieldPath;
+  }
+
+  public String getColumnName() {
+    return columnName;
+  }
+
+  public String getFieldPath() {
+    return fieldPath;
+  }
+}
